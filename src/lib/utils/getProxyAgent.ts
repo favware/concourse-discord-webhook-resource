52b35dae7707ebca0a889bbdbbeefa155bcbe76d
Jeroen Claassens <support@favware.tech>
2024-10-19 12:11:02 +0200

    chore: s/import/include/

--- src/lib/utils/getProxyAgent.ts
+++ src/lib/utils/getProxyAgent.ts
@@ -1,5 +1,5 @@
-import { ProxyAgent } from 'undici';
 import type { CheckedOptionsType } from '#lib/utils/preflightChecks';
+import { ProxyAgent } from 'undici';
 
 export function getProxyAgent(options: CheckedOptionsType): ProxyAgent | null {
 	if (!options.proxy && !options['allow-insecure']) return null;
